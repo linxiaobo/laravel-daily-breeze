e9c28da086f051422416d16b6befc60a7ad3933a
xiaobo lin <xiaobolin@xiaobos-MacBook-Pro.local>
2025-03-19 15:53:25 +0800

    categories crud

---
+++ resources/views/categories/create.blade.php
@@ -0,0 +1,28 @@
+<x-app-layout>
+    <x-slot name="header">
+        <h2 class="font-semibold text-xl text-gray-800 leading-tight">
+            {{ __('Category Create') }}
+        </h2>
+    </x-slot>
+
+    <div class="py-12">
+        <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
+            <div class="bg-white overflow-hidden shadow-sm sm:rounded-lg">
+                <div class="p-6 text-gray-900">
+                    <form method="POST" action={{ route('categories.store') }}>
+                        @csrf
+                        <div>
+                            <div>
+                                <label for="name">Name:</label>
+                            </div>
+                            <x-text-input name="name" value=""></x-text-input>
+                        </div>
+                        <div>
+                            <x-primary-button>Save</x-primary-button>
+                        </div>
+                    </form>
+                </div>
+            </div>
+        </div>
+    </div>
+</x-app-layout>
---
+++ resources/views/categories/edit.blade.php
@@ -0,0 +1,29 @@
+<x-app-layout>
+    <x-slot name="header">
+        <h2 class="font-semibold text-xl text-gray-800 leading-tight">
+            {{ __('Categories') }}
+        </h2>
+    </x-slot>
+
+    <div class="py-12">
+        <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
+            <div class="bg-white overflow-hidden shadow-sm sm:rounded-lg">
+                <div class="p-6 text-gray-900">
+                    <form method="POST" action={{ route('categories.update', $category) }}>
+                        @csrf
+                        @method('PUT')
+                        <div>
+                            <div>
+                                <label for="name">Name:</label>
+                            </div>
+                            <x-text-input name="name" value="{{ $category->name }}"></x-text-input>
+                        </div>
+                        <div>
+                            <x-primary-button>Save</x-primary-button>
+                        </div>
+                    </form>
+                </div>
+            </div>
+        </div>
+    </div>
+</x-app-layout>
